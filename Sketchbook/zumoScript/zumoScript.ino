93640185eaad9052c1abb4ae304560fb5a9b9511
Echo319 <33762083+Echo319@users.noreply.github.com>
2019-01-07 17:01:46 +0000

    first draft of automatic mode

--- Sketchbook/zumoScript/zumoScript.ino
+++ Sketchbook/zumoScript/zumoScript.ino
@@ -4,9 +4,13 @@
 #define TRIGGER_PIN  6 // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     2  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 100 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define NUM_SENSORS 6
 
 ZumoMotors motors;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+unsigned int sensor_values[NUM_SENSORS];
+ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 
 int speed = 100; // max speed is 255
 bool object = false;
@@ -81,6 +85,34 @@ void left (int speed)
 void right (int speed)
 {
   motors.setSpeeds(speed, -speed);
+}
+
+void autoMode() {
+  bool blocked = false;
+  while(blocked == false) { 
+      if (sensor_values[0] < QTR_THRESHOLD) {
+        // if leftmost sensor detects line, reverse and turn to the right
+        reverse(speed);
+        delay(REVERSE_DURATION);
+        right(speed);
+        delay(TURN_DURATION);
+        forward(speed);
+      } else if (sensor_values[5] < QTR_THRESHOLD) {
+        // if rightmost sensor detects line, reverse and turn to the left
+        reverse(speed);
+        delay(REVERSE_DURATION);
+        left(speed);
+        delay(TURN_DURATION);
+        forward(speed);
+      } else if (sensor_values[2] < QTR_THRESHOLD || seonsor_values[3] < QTR_THRESHOLD ) {
+        // if the middle sensors detect line, stop
+        stop();
+        Serial.println("Obstruction"); 
+      } else {
+        // otherwise, go straight
+        forward(speed);
+      }
+    }
 }
 
 void roomRight() {
