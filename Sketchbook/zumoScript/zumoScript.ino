2a3efa2580fed82a7e9040a5c6b0edad65cb7635
Echo319 <33762083+Echo319@users.noreply.github.com>
2019-01-24 19:45:28 +0000

    Created a basic ui with wasd controls to move the zumo manually
    Using a StateChangeListener and a KeyListener to have the wasd keys and the buttons move the zumo on press and stop it on release

---
+++ Sketchbook/zumoScript/zumoScript.ino
@@ -0,0 +1,64 @@
+#include <ZumoShield.h>
+
+ZumoMotors motors;
+int speed = 100; // max speed is 255
+void setup(void)
+{
+  int i;
+  for(i=5;i<=8;i++)
+  pinMode(i, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("setup");
+}
+
+void loop(void)
+{
+  while (Serial.available() < 1) {} // Wait until a character is received
+  char val = Serial.read();
+  switch(val) {
+    case 'w'://Move Forward
+    case 'W':
+      forward (speed);
+      break;
+    case 's'://Move Backwards
+    case 'S':
+      reverse (speed);
+      break;
+    case 'a'://Turn Left
+    case 'A':
+      left (speed);
+      break;
+    case 'd'://Turn Right
+    case 'D':
+      right (speed);
+      break;
+    default:
+      stop();
+      break;
+  }
+}
+
+void stop(void) //Stop
+{
+  motors.setSpeeds(0,0);
+}
+
+void forward(int speed)
+{
+  motors.setSpeeds(speed, speed);
+}
+
+void reverse (int speed)
+{
+  motors.setSpeeds(-speed, -speed);
+}
+
+void left (int speed)
+{
+  motors.setSpeeds(-speed, speed);
+}
+
+void right (int speed)
+{
+  motors.setSpeeds(speed, -speed);
+}
