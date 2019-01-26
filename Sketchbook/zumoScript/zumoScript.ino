bfcf73d21c7010e50a656232f3f171edddda3a5b
Echo319 <33762083+Echo319@users.noreply.github.com>
2019-01-26 21:51:15 +0000

    Added a compass the idea is to use it to ensure turns are 90 degrees

--- Sketchbook/zumoScript/zumoScript.ino
+++ Sketchbook/zumoScript/zumoScript.ino
@@ -5,7 +5,12 @@
 #define ECHO_PIN     2  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 100 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define QTR_THRESHOLD 400
+// constants for compass 
+#define CALIBRATION_SAMPLES 70  // Number of compass readings to take when calibrating
+#define CRB_REG_M_2_5GAUSS 0x60 // CRB_REG_M value for magnetometer +/-2.5 gauss full scale
+#define CRA_REG_M_220HZ    0x1C // CRA_REG_M value for magnetometer 220 Hz update rate
 
+LSM303 compass;
 ZumoMotors motors;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 ZumoReflectanceSensorArray reflectanceSensors;
@@ -21,17 +26,8 @@ int ends = 0;
 
 void setup(void)
 {
-  //initialise and calibrate reflectance sensors
-  reflectanceSensors.init();
-  delay(500);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);        // turn on LED to indicate we are in calibration mode
-  unsigned long startTime = millis();
-  while(millis() - startTime < 5000)   // make the calibration take 10 seconds
-  {
-    reflectanceSensors.calibrate();
-  }
-  digitalWrite(13, LOW);         // turn off LED to indicate we are through with calibration
+  calibrateRSA();
+  calibrateCompass();
   
   int i;
   for(i=5;i<=8;i++)
@@ -222,4 +218,134 @@ void clearSerial() {
   while(Serial.available()) {
     Serial.read();
   }
+}
+
+
+// RSA code comes from Calibrate sensor example of Zumo library
+void calibrateRSA() {
+    //initialise and calibrate reflectance sensors
+  reflectanceSensors.init();
+  delay(500);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);        // turn on LED to indicate we are in calibration mode
+  unsigned long startTime = millis();
+  while(millis() - startTime < 5000)   // make the calibration take 10 seconds
+  {
+    reflectanceSensors.calibrate();
+  }
+  digitalWrite(13, LOW);         // turn off LED to indicate we are through with calibration
+}
+
+// All compass code was adapted from the compass example of the zumo library mostly the same however
+// its also a complete mess
+void calibrateCompass() {
+  // The highest possible magnetic value to read in any direction is 2047
+  // The lowest possible magnetic value to read in any direction is -2047
+  LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32767, -32767, -32767};
+  unsigned char index;
+  // Initiate LSM303
+  compass.init();
+  // Enables accelerometer and magnetometer
+  compass.enableDefault();
+  compass.writeReg(LSM303::CRB_REG_M, CRB_REG_M_2_5GAUSS); // +/- 2.5 gauss sensitivity to hopefully avoid overflow problems
+  compass.writeReg(LSM303::CRA_REG_M, CRA_REG_M_220HZ);    // 220 Hz compass update rate
+
+  // To calibrate the magnetometer, the Zumo spins to find the max/min
+  // magnetic vectors. This information is used to correct for offsets
+  // in the magnetometer data.
+  motors.setLeftSpeed(speed);
+  motors.setRightSpeed(-speed);
+  for(index = 0; index < CALIBRATION_SAMPLES; index ++)
+  {
+    // Take a reading of the magnetic vector and store it in compass.m
+    compass.read();
+    running_min.x = min(running_min.x, compass.m.x);
+    running_min.y = min(running_min.y, compass.m.y);
+    running_max.x = max(running_max.x, compass.m.x);
+    running_max.y = max(running_max.y, compass.m.y);
+    delay(50);
+  }
+
+  motors.setLeftSpeed(0);
+  motors.setRightSpeed(0);
+  // Set calibrated values to compass.m_max and compass.m_min
+  compass.m_max.x = running_max.x;
+  compass.m_max.y = running_max.y;
+  compass.m_min.x = running_min.x;
+  compass.m_min.y = running_min.y;
+}
+
+// Converts x and y components of a vector to a heading in degrees.
+// This function is used instead of LSM303::heading() because we don't
+// want the acceleration of the Zumo to factor spuriously into the
+// tilt compensation that LSM303::heading() performs. This calculation
+// assumes that the Zumo is always level.
+template <typename T> float heading(LSM303::vector<T> v)
+{
+  float x_scaled =  2.0*(float)(v.x - compass.m_min.x) / ( compass.m_max.x - compass.m_min.x) - 1.0;
+  float y_scaled =  2.0*(float)(v.y - compass.m_min.y) / (compass.m_max.y - compass.m_min.y) - 1.0;
+
+  float angle = atan2(y_scaled, x_scaled)*180 / M_PI;
+  if (angle < 0)
+    angle += 360;
+  return angle;
+}
+
+// Yields the angle difference in degrees between two headings
+float relativeHeading(float heading_from, float heading_to)
+{
+  float relative_heading = heading_to - heading_from;
+
+  // constrain to -180 to 180 degree range
+  if (relative_heading > 180)
+    relative_heading -= 360;
+  if (relative_heading < -180)
+    relative_heading += 360;
+
+  return relative_heading;
+}
+
+// Average 10 vectors to get a better measurement and help smooth out
+// the motors' magnetic interference.
+float averageHeading()
+{
+  LSM303::vector<int32_t> avg = {0, 0, 0};
+
+  for(int i = 0; i < 10; i ++)
+  {
+    compass.read();
+    avg.x += compass.m.x;
+    avg.y += compass.m.y;
+  }
+  avg.x /= 10.0;
+  avg.y /= 10.0;
+
+  // avg is the average measure of the magnetic vector.
+  return heading(avg);
+}
+
+
+void turnDegrees(int angle) {
+    float heading, relative_heading;
+    int comSpeed;
+    static float target_heading = target_heading = fmod(averageHeading() + angle, 360);
+    while(abs(relative_heading) < 2) {
+      // Heading is given in degrees away from the magnetic vector, increasing clockwise
+      heading = averageHeading();
+      // This gives us the relative heading with respect to the target angle
+      relative_heading = relativeHeading(heading, target_heading);
+      // To avoid overshooting, the closer the Zumo gets to the target
+      // heading, the slower it should turn. Set the motor speeds to a
+      // minimum base amount plus an additional variable amount based
+      // on the heading difference.
+      comSpeed = speed*relative_heading/180;
+      if (comSpeed < 0)
+        comSpeed -= (speed * 0.5);
+        else
+        comSpeed += (speed * 0.5);
+      motors.setSpeeds(comSpeed, -comSpeed);
+    }
+    // Turn off motors and wait a short time to reduce interference from motors
+    motors.setSpeeds(0, 0);
+    delay(100);
 }
